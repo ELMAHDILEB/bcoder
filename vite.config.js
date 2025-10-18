8434e45e486c9ff4bfecc9746abab9daa58edbf5
ELMAHDILEB <118133055+ELMAHDILEB@users.noreply.github.com>
2025-10-18 12:55:33 +0000

    add sitemap.xml to public folder and update related configs

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,14 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import ViteSitemap  from 'vite-plugin-sitemap';
 
 export default defineConfig({
   plugins: [
     react(),
+    ViteSitemap({
+      hostname: 'https://www.bcoder.codes/', 
+      routes: ['/','/about','/projects','/contact','/testimonials'],
+    }),
   ],
   
 });
